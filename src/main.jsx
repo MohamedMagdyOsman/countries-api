d25e6778517a97ad162377a08e09517c0a66d613
Mohamed Magdy <magdyofficial3@gmail.com>
2024-05-06 11:45:34 +0300

    fix basename path

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/countries-api'>
+    <BrowserRouter basename='/countries-api/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
